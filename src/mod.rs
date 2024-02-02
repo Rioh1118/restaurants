f93994d528d506e9d56af0c0cc500351d57263f0
Rioh1118 <rio2515t@gmail.com>
2024-02-03 00:34:48 +0900

    add dir and api

---
+++ src/mod.rs
@@ -0,0 +1,5 @@
+pub mod db;
+pub mod models;
+pub mod schema;
+pub mod routes;
+
---
+++ src/routes/api_client/api_keys.rs
@@ -0,0 +1,36 @@
+use dotenv::dotenv;
+use std::env;
+pub enum Key {
+    GooglePlaces(String),
+}
+
+pub enum Api {
+    GooglePlaces,
+}
+
+pub fn get_api_key (api: Api) -> Result<Key, std::env::VarError> {
+    dotenv().ok();
+    match api {
+        Api::GooglePlaces => {
+            let key = env::var("GOOGLE_PLACES_API_KEY")?;
+            Ok(Key::GooglePlaces(key))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_api_key() {
+        env::set_var("GOOGLE_PLACES_API_KEY", "test_key");
+
+        let result = get_api_key(Api::GooglePlaces);
+
+        match result {
+            Ok(Key::GooglePlaces(result)) => assert_eq!(result, "test_key"),
+            _ => panic!("Unexpected result"),
+        }
+    }   
+}
